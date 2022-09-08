dd0639b9b27cf56f918ff235cdb5a2c17361f3e4
Tanish Jain <tanishjain@tanishs-mbp.myfiosgateway.com>
2022-09-07 20:36:21 -0400

    adding

---
+++ Code/predict.py
@@ -0,0 +1,47 @@
+from keras.models import load_model
+import pandas as pd
+import numpy as np
+
+
+
+class Predict(object):
+
+    def __init__(self):
+        self.model=load_model('bbb_model')
+
+
+    def preprocess_data(self,data):
+        if type(data)==np.ndarray:
+            data=np.float32(data)
+            return self.normalize_data(data,False)
+        elif type(data)==list():
+            return self.normalize_data(np.array([data]),False)
+
+
+
+
+    def normalize_data(self,data,flag=None):
+        if flag:
+            mean=np.load('mean.npy')
+            std=np.load('std.npy')
+
+            data=(data-mean)/std
+            return data
+
+        else:
+            return data
+
+
+    def predictlogBB(self,data):
+        normalizeddata=self.preprocess_data(data)
+        logBBresult=self.model.predict(normalizeddata.reshape(-1,16))
+        print("Results predicted")
+        return logBBresult[0][0]
+
+
+if __name__=="__main__":
+    logBBpredictor=Predict()
+
+    data=pd.read_csv('../Full_Dataset.csv')
+    test=data.iloc[5,:-2]
+    print(logBBpredictor.predictlogBB(test.values))
